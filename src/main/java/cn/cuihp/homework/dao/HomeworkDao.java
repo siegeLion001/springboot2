c845a4ae36d02ee73363bf100ffe00e974dfdc33
siegeLion001 <978176070@qq.com>
2018-09-14 14:56:36 +0800

    dao提交

--- src/main/java/cn/cuihp/homework/dao/HomeworkDao.java
+++ src/main/java/cn/cuihp/homework/dao/HomeworkDao.java
@@ -3,11 +3,11 @@ package cn.cuihp.homework.dao;
 import java.util.List;
 import java.util.Map;
 
-import com.lhh.modules.homework.domain.ExcellentHomeworkVo;
-import com.lhh.modules.homework.domain.HomeworkVo;
-import com.lhh.modules.homework.domain.InsertHomeWorkVo;
-import com.lhh.modules.homework.entity.HomeworkEntity;
-import com.lhh.modules.sys.dao.BaseDao;
+import cn.cuihp.homework.domain.ExcellentHomeworkVo;
+import cn.cuihp.homework.domain.HomeworkVo;
+import cn.cuihp.homework.domain.InsertHomeWorkVo;
+import cn.cuihp.homework.entity.HomeworkEntity;
+
 
 /**
  * 作业表
@@ -16,7 +16,32 @@ import com.lhh.modules.sys.dao.BaseDao;
  * @email miaof@lanhaihui.net
  * @date 2018-05-18 09:44:45
  */
-public interface HomeworkDao extends BaseDao<HomeworkEntity> {
+public interface HomeworkDao  {
+    void save(HomeworkEntity t);
+
+    void save(Map<String, Object> map);
+
+    void saveBatch(List<HomeworkEntity> list);
+
+    int update(HomeworkEntity t);
+
+    int update(Map<String, Object> map);
+
+    int delete(Object id);
+
+    int delete(Map<String, Object> map);
+
+    int deleteBatch(Object[] id);
+
+    HomeworkEntity queryObject(Object id);
+
+    List<HomeworkEntity> queryList(Map<String, Object> map);
+
+    List<HomeworkEntity> queryList(Object id);
+
+    int queryTotal(Map<String, Object> map);
+
+    int queryTotal();
     List<InsertHomeWorkVo> queryList4Map(Map<String, Object> params);
 
     HomeworkEntity queryContent(Integer homeworkId);
